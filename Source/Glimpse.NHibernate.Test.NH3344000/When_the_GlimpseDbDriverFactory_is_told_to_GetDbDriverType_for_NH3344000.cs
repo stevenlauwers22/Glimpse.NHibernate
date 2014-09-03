15050f80e4f3fbc0ce49b78fd7b992648406d0ee
stevenlauwers22 <stevenlauwers22@hotmail.com>
2014-09-03 14:05:40 +0200

    Add tests for NH 3.3.4.4000

---
+++ Source/Glimpse.NHibernate.Test.NH3344000/When_the_GlimpseDbDriverFactory_is_told_to_GetDbDriverType_for_NH3344000.cs
@@ -0,0 +1,12 @@
+﻿namespace Glimpse.NHibernate.Test.NH3344000
+{
+    public class When_the_GlimpseDbDriverFactory_is_told_to_GetDbDriverType_for_NH3344000
+        : When_the_GlimpseDbDriverFactory_is_told_to_GetDbDriverType
+    {
+        protected override string GetExpectedVersionNumber()
+        {
+            // 3344000 is a drop in replacement for 3314000
+            return "3314000";
+        }
+    }
+}
